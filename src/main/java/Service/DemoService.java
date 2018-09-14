bf9e4aafab7bbeb64fb19e722bbc856e8f575073
D070550 <D070550@WDFN34222702A.emea.global.corp.sap>
2018-09-14 10:27:55 +0200

    next level put

--- src/main/java/Service/DemoService.java
+++ src/main/java/Service/DemoService.java
@@ -8,6 +8,7 @@ import javax.annotation.security.PermitAll;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -154,7 +155,7 @@ public class DemoService {
 
 	}
 
-	@GET
+	@PUT
 	@Path("angebotkaufen/{menge}/{angebotsid}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Object kaufeAngebot(@PathParam("menge") int menge, @PathParam("angebotsid") int id) {
