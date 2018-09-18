36d280ed42e3369b3de7d941107c2496a0839881
Felix <Felix@192.168.0.45>
2018-09-18 22:49:45 +0200

    bug fix

--- src/main/java/DTOs/MaterialienGesamtDTO.java
+++ src/main/java/DTOs/MaterialienGesamtDTO.java
@@ -1,26 +1,26 @@
-package DTOs;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import Fachkonzept.Material;
-
-public class MaterialienGesamtDTO {
-
-	public MaterialienGesamtDTO(Map<String, Integer> maschinenmatrix) {
-	    for (Map.Entry<String, Integer> ein : maschinenmatrix.entrySet()) {
-	    	materialien.add(new MaterialDTO((Material)Material.findeMarkteinheit(ein.getKey(), Material.class), ein.getValue()));
-	    }
-	}
-	
-	private List<MaterialDTO> materialien = new ArrayList<MaterialDTO>();
-
-	public List<MaterialDTO> getMaschinen() {
-		return materialien;
-	}
-	
-	
-	
-
-}
+//package DTOs;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//import Fachkonzept.Material;
+//
+//public class MaterialienGesamtDTO {
+//
+//	public MaterialienGesamtDTO(Map<String, Integer> materialmatrix) {
+//	    for (Map.Entry<String, Integer> ein : materialmatrix.entrySet()) {
+//	    	materialien.add(new MaterialDTO((Material)Material.findeMarkteinheit(ein.getKey(), Material.class), ein.getValue()));
+//	    }
+//	}
+//	
+//	private List<MaterialDTO> materialien = new ArrayList<MaterialDTO>();
+//
+//	public List<MaterialDTO> getMaterialien() {
+//		return materialien;
+//	}
+//	
+//	
+//	
+//
+//}
--- src/main/java/DTOs/ProdukteGesamtDTO.java
+++ src/main/java/DTOs/ProdukteGesamtDTO.java
@@ -1,22 +1,22 @@
-package DTOs;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import Fachkonzept.Produkt;
-
-public class ProdukteGesamtDTO {
-
-	public ProdukteGesamtDTO(Map<String, Integer> produktmatrix) {
-	    for (Map.Entry<String, Integer> ein : produktmatrix.entrySet()) {
-	    	produkte.add(new ProduktDTO(Produkt.findeProdukt(ein.getKey()), ein.getValue()));
-	    }
-	}
-	
-	private List<ProduktDTO> produkte = new ArrayList<ProduktDTO>();
-
-	public List<ProduktDTO> getMaschinen() {
-		return produkte;
-	}
-}
+//package DTOs;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//import Fachkonzept.Produkt;
+//
+//public class ProdukteGesamtDTO {
+//
+//	public ProdukteGesamtDTO(Map<String, Integer> produktmatrix) {
+//	    for (Map.Entry<String, Integer> ein : produktmatrix.entrySet()) {
+//	    	produkte.add(new ProduktDTO(Produkt.findeProdukt(ein.getKey()), ein.getValue()));
+//	    }
+//	}
+//	
+//	private List<ProduktDTO> produkte = new ArrayList<ProduktDTO>();
+//
+//	public List<ProduktDTO> getProdukte() {
+//		return produkte;
+//	}
+//}
