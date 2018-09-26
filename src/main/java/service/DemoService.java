181283edb48303e5c895fdc8cb8201616db9959b
D070430 <D070430@WDFN34222638A.emea.global.corp.sap>
2018-09-26 13:43:28 +0200

    unused imports

--- src/main/java/service/DemoService.java
+++ src/main/java/service/DemoService.java
@@ -11,10 +11,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
-
-import org.glassfish.jersey.server.JSONP;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
