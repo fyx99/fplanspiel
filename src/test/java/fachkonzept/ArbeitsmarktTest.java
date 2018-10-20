a83e3309c4e77ced610e3e1a514d182acc3535ee
D070423 <D070423@WDFN34222730A.emea.global.corp.sap>
2018-10-20 12:40:13 +0200

    TestTestTest

--- src/test/java/fachkonzept/ArbeitsmarktTest.java
+++ src/test/java/fachkonzept/ArbeitsmarktTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import fachkonzept.markt.Arbeitsmarkt;
 import fachkonzept.markt.Beschaffungsmarkt;
+import fachkonzept.markt.Finanzmarkt;
+import fachkonzept.util.KreditArt;
 import fachkonzept.util.MaterialArt;
 import fachkonzept.util.MitarbeiterFachgebiet;
 
@@ -60,4 +62,30 @@ public class ArbeitsmarktTest {
         assertEquals(269, am.getAngebote().get(1).getPreis());
 //        assertEquals(-3300, kk.getGuv().rundenErgebnis());
 	}
+	
+	@Test
+	void umsatzProMitarbeiterFachgebiet() {
+		int size = Arbeitsmarkt.getUmsatzHistorie().size();
+		int size2 = Arbeitsmarkt.umsatzProMitarbeiterFachgebiet(MitarbeiterFachgebiet.MASCHINE).size();
+		int size3 = Arbeitsmarkt.umsatzProMitarbeiterFachgebiet(MitarbeiterFachgebiet.VERTRIEB).size();
+		Arbeitsmarkt amarkt = new Arbeitsmarkt();
+		Angebot a1 = new Angebot(new Mitarbeiter("test", 300, 120000, MitarbeiterFachgebiet.MASCHINE),22,323);
+		Angebot a2 = new Angebot(new Mitarbeiter("test2", 300, 120000, MitarbeiterFachgebiet.VERTRIEB),22,323);
+		amarkt.anbieten(a1);
+		amarkt.anbieten(a2);
+		
+		Finanzmarkt fmarkt = new Finanzmarkt();
+        Angebot a3 = new Angebot(new Kredit(1555,0.05, 5,KreditArt.Mehr_Cash), 22, 11);
+        fmarkt.anbieten(a3);
+		
+        amarkt.kaufen(a1, 10, new Unternehmen("a1", new Spiel()));
+        amarkt.kaufen(a1, 15, new Unternehmen("a1", new Spiel()));
+        amarkt.kaufen(a2, 10, new Unternehmen("a1", new Spiel()));
+        fmarkt.kaufen(a3, 10, new Unternehmen("a1", new Spiel()));
+        
+        assertEquals(4 + size, Arbeitsmarkt.getUmsatzHistorie().size());
+    	assertEquals(1 + size2, Arbeitsmarkt.umsatzProMitarbeiterFachgebiet(MitarbeiterFachgebiet.MASCHINE).size());
+    	assertEquals(2 + size3, Arbeitsmarkt.umsatzProMitarbeiterFachgebiet(MitarbeiterFachgebiet.MASCHINE).size());
+	
+	}	
 }
--- src/test/java/fachkonzept/FinanzmarktTest.java
+++ src/test/java/fachkonzept/FinanzmarktTest.java
@@ -48,5 +48,27 @@ class FinanzmarktTest {
         assertEquals(1000, testU.getVerbindlichkeiten().get(0).getKredit().getVolumen());
     }
     
+    @Test 
+    void umsatzProKreditArt() {
+    	int size = Finanzmarkt.getUmsatzHistorie().size();
+    	int size2 = Finanzmarkt.umsatzProKreditArt(KreditArt.Kurzes_Cash).size();
+    	int size3 = Finanzmarkt.umsatzProKreditArt(KreditArt.Mehr_Cash).size();
+    	Finanzmarkt fmarkt = new Finanzmarkt();
+    	
+    	Angebot a1 = new Angebot(new Kredit(1000, 0.05, 10, KreditArt.Kurzes_Cash), 22, 10);
+    	Angebot a2 = new Angebot(new Kredit(1555,0.05, 5,KreditArt.Mehr_Cash), 22, 11);
+    	fmarkt.anbieten(a1);
+    	fmarkt.anbieten(a2);
+    	
+    	fmarkt.kaufen(a1, 2,new Unternehmen("a1", new Spiel()));
+    	fmarkt.kaufen(a1, 1, new Unternehmen("a1", new Spiel()));
+    	fmarkt.kaufen(a2, 1, new Unternehmen("a2", new Spiel()));
+    	
+    	assertEquals(size, Finanzmarkt.getUmsatzHistorie().size());
+    	assertEquals(1+size2, Finanzmarkt.umsatzProKreditArt(KreditArt.Kurzes_Cash).size());
+    	assertEquals(2+size3, Finanzmarkt.umsatzProKreditArt(KreditArt.Mehr_Cash).size());
+    	
+    }
+    
 
 }
